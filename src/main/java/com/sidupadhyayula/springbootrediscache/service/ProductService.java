7298651c2a53c8b4f1a98c7ea14c6627b99b145a
Sai Siddharth Upadhyayula <usaisiddarth@gmail.com>
2025-03-17 23:48:27 -0400

    Integration tests to test the functionality

--- src/main/java/com/sidupadhyayula/springbootrediscache/service/ProductService.java
+++ src/main/java/com/sidupadhyayula/springbootrediscache/service/ProductService.java
@@ -11,6 +11,7 @@ import org.springframework.stereotype.Service;
 @Service
 public class ProductService {
 
+    public static final String PRODUCT_CACHE = "products";
     private final ProductRepository productRepository;
 
 
